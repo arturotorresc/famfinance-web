4d097b5ca465ec03cd9562e33b47d394c8b0a058
Arturo Torres <arturotc22@gmail.com>
2021-03-15 17:59:05 -0600

    Removed unused option

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,17 +1,7 @@
-import { useRouter } from "next/router";
 import { useContext } from "react";
 import { CurrentUserContext } from "../context/CurrentUserContext";
 
-interface ICurrentUserOptions {
-  redirectTo: string;
-}
-
-export function useCurrentUser(opts?: ICurrentUserOptions) {
+export function useCurrentUser() {
   const value = useContext(CurrentUserContext);
-  const router = useRouter();
-  const notLoggedIn = value.user === null || value.user === undefined;
-  if (opts && notLoggedIn) {
-    router.push(opts.redirectTo);
-  }
   return value;
 }
