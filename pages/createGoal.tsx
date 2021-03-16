18319354a4ebc216af857b48deab78a213c0544d
Fiacro <fgrl1408@gmail.com>
2021-03-16 16:55:51 -0600

    Adding createGoal route

---
+++ pages/createGoal.tsx
@@ -0,0 +1,10 @@
+import CreateGoalPage from "../components/templates/CreateGoalPage";
+import PublicLayout from "../components/layouts/PublicLayout";
+
+export default function Goal() {
+  return (
+    <PublicLayout>
+      <CreateGoalPage />
+    </PublicLayout>
+  );
+}
