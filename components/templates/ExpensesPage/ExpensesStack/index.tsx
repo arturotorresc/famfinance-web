42cf377e1f94122d90cbe075a187b2f31966876c
Fiacro <fgrl1408@gmail.com>
2021-05-02 20:20:16 -0500

    Expenses showing frequency

---
+++ components/templates/ExpensesPage/ExpensesStack/index.tsx
@@ -0,0 +1,96 @@
+import { Stack, useToast } from "@chakra-ui/react";
+import ExpensesBox from "../ExpensesBox";
+import { useRouter } from "next/router";
+import fetcher from "../../../../fetchers/fetcher";
+import { useQuery, useMutation } from "react-query";
+
+interface IFrequency {
+    _id: string;
+    frequencyType: string;
+    day: number;
+    weekDay: string;
+    weeksRepeat: number;
+    monthsRepeat: number;
+    months: string[];
+    startEndMonth: string;
+}
+
+interface IExpense {
+  _id: string;
+  title: string;
+  from: string;
+  until: string;
+  qty: number;
+  category: string;
+  frequency: IFrequency;
+}
+
+interface IData {
+  expense: IExpense[];
+}
+
+interface IKey {
+  _id: string;
+}
+
+export default function ExpensesStack() {
+  const router = useRouter();
+
+  const { status, data, error, isFetching, refetch } = useQuery(
+    "expenses",
+    async () => {
+      const { data } = await fetcher.get<IData>("/expense");
+      return data;
+    }
+  );
+
+  console.log(data);
+
+  const toast = useToast();
+
+  const deleteMutation = useMutation((id) => fetcher.delete(`/expense/${id}`));
+
+  function onDeleteClicked(key: string) {
+    deleteMutation.mutate(key, 
+          {
+            onSuccess: () => {
+                refetch();
+                toast({
+                  status: "success",
+                  title: "Gasto Borrado",
+                  description: `El Gasto fue Borrado Exitosamente`,
+                });
+              },
+              onError: () => {
+                toast({
+                  status: "error",
+                  title: "Oops! Algo Ocurrio!",
+                  description: "Por favor, intenta de nuevo!",
+                });
+              },
+        }
+    )
+}
+
+  return (
+    <Stack>
+      {isFetching ? (
+        <div>Recuperando la información ...</div>
+      ) : (
+        data?.expense.map((expense) => (
+          <ExpensesBox
+            onDeleteClicked= {onDeleteClicked}
+            key={expense._id}
+            _id={expense._id}
+            title={expense.title}
+            from={expense.from}
+            until={expense.until}
+            qty={expense.qty}
+            category={expense.category}
+            frequency={expense.frequency}
+          />
+        ))
+      )}
+    </Stack>
+  );
+}
