b5a75c470d52fcb1bcbe6108f866d7a038565f23
Arturo Torres <arturotc22@gmail.com>
2021-05-26 11:11:22 -0500

    Public env vars

--- fetchers/serverSideFetcher.ts
+++ fetchers/serverSideFetcher.ts
@@ -4,7 +4,7 @@ import { GetServerSidePropsContext } from "next";
 export function createServerSideFetcher(ctx: GetServerSidePropsContext) {
   const cookie = ctx.req.headers.cookie;
   const fetcher = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
     headers: {
       cookie,
