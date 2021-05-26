c58b6b4680f92013b7e0e11fbe65157c9beb4df4
Arturo Torres <arturotc22@gmail.com>
2021-05-26 11:10:43 -0500

    Added env variab

--- fetchers/serverSideFetcher.ts
+++ fetchers/serverSideFetcher.ts
@@ -4,7 +4,7 @@ import { GetServerSidePropsContext } from "next";
 export function createServerSideFetcher(ctx: GetServerSidePropsContext) {
   const cookie = ctx.req.headers.cookie;
   const fetcher = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: process.env.BACKEND_URL,
     withCredentials: true,
     headers: {
       cookie,
